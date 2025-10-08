e791c93f152da1b70676746e3ef4edbaabffd555
Fojla Rabbi <fojlarabbi018@gmail.com>
2025-10-08 21:47:17 +0600

    Create Prime Factorization.cpp

---
+++ Number Theory/Prime Factorization.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> v;
+    int i = 2;
+    while(n > 1) {
+        if(n % i == 0) {
+            v.push_back(i); // i is guaranteed to be prime
+            n = n / i; // Quotient
+            i = 2;
+        } 
+        else i++;
+    }
+    sort(v.begin(), v.end());
+    for(auto i: v) {
+        cout << i << " ";
+    }
+    cout << '\n';
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
