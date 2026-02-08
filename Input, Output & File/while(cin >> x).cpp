3ba782bcb8ab37ec31b567481a977a6de38b2b8f
Fojla Rabbi <fojlarabbi018@gmail.com>
2026-02-08 17:10:21 +0600

    Create while(cin >> x).cpp

---
+++ Input, Output & File/while(cin >> x).cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+    int x;
+    while(cin >> x) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
+// When you don't know the size of the array and 
+// you are told to take every input written in the input file
+
+// input: 12 13 14 15
+
+// Try it for --> 1h2 13 14 15
+// then you will see a magic
