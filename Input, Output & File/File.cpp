b6ce9bab80e9764cfee906ee121a4948c2267d31
Fojla Rabbi <fojlarabbi018@gmail.com>
2026-02-08 16:49:38 +0600

    Create File.cpp

---
+++ Input, Output & File/File.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+    freopen("custom_input.txt", "r", stdin);
+    freopen("custom_out.txt", "w", stdout);
+    int x;
+    cin >> x; // Input will be taken from "custom_input.txt" file
+    cout << x; // Output will be shown in "custom_out.txt" file
+
+    return 0;
+}
