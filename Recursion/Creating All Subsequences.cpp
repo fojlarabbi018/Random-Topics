f0ad5bbc0fddca597137639b8c31355ebe7ad956
Fojla Rabbi <fojlarabbi018@gmail.com>
2026-02-09 15:23:21 +0600

    Update Creating All Subsequences.cpp

--- Recursion/Creating All Subsequences.cpp
+++ Recursion/Creating All Subsequences.cpp
@@ -1,34 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n; 
-// A good example of global declaration
-int arr[100];
-bool is_taken[100]={false};
-void sub_sq(int pos){
-	// Base case
-	if(pos>n){
-		for(int i=1;i<=n;i++){
-			if(is_taken[i]) cout<<arr[i]<<" ";
-		}
-		cout<<"\n";
-		return;
-	}
-	is_taken[pos]=false;
-	sub_sq(pos+1);
-	// Not taking the current element
-	// Without taking that element, compute all possible combinations
-	// for the next whole part
+#define ll long long
 
-	// After that, take that element and do the same
-	is_taken[pos]=true;
-	sub_sq(pos+1);
+int n;
+vector<int> a;
+vector<bool> is_taken;
+
+void rec(int pos) {
+    if(pos > n) {
+        for(int i = 1; i <= n; i++) {
+            if(is_taken[i]) {
+                cout << a[i] << " ";
+            }
+        }
+        cout << '\n';
+        return;
+    }
+
+    is_taken[pos] = false;
+    rec(pos + 1);
+
+    is_taken[pos] = true;
+    rec(pos + 1);
 }
-int main(){
-	cin>>n;
-	for(int i=1;i<=n;i++){
-		cin>>arr[i];
-	}
-	sub_sq(1);
-	
-	return 0;
+
+void solve() {
+    cin >> n;
+    a.resize(n + 1);
+    is_taken.resize(n + 1, false);
+    for(int i = 1; i <= n; i++) cin >> a[i];
+
+    rec(1);
 }
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    solve();
+    return 0;
+}
+
+/* 
+Subsequences follow an awesome recurrence relation
+Say you have given an array a = 1, 2, 3, 4, 5 (as we need index, we took same values same as index)
+now say you have only 4, 5 then there will be [{}, {5}, {4}, {4, 5}] these seqs
+now when you add an extra element like 3 on the left of 4, 5 then the prev 4 remains the same
+(seqs without 3) and extra 4 will be created by adding 3 on the left of each seq
+*/
