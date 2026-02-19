9dd2e904ae921ed60b6304501ce916b76014ddee
Fojla Rabbi <fojlarabbi018@gmail.com>
2026-02-19 17:13:53 +0600

    Update Main Template.cpp

--- Main Template.cpp
+++ Main Template.cpp
@@ -31,6 +31,7 @@ unordered_map<long long int, int, custom_hash> mp;
 #include<ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 using namespace std;
+#define ll long long
 
 template <typename T> using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
