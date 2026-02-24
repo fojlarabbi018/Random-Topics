da2f4ed13846ce1e5b6343615497fa28ee53f7d4
Fojla Rabbi <fojlarabbi018@gmail.com>
2026-02-24 21:41:22 +0600

    Update Topological Sort Kahn's Algorithm.cpp

--- Algorithms/Topological Sort Kahn's Algorithm.cpp
+++ Algorithms/Topological Sort Kahn's Algorithm.cpp
@@ -1,1 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
 
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> adj(n);
+    vector<int> indegree(n, 0);
+    for(int i = 0; i < m; i++) {
+        int u, v;
+        adj[u].push_back(v);
+        indegree[v]++;
+    }
+
+    queue<int> q;
+    for(int i = 0; i < n; i++) {
+        if(indegree[i] == 0) q.push(i);
+    }
+
+    while(!q.empty()) {
+        int cur = q.front();
+        q.pop();
+        cout << cur << " ";
+        for(auto next: adj[cur]) {
+            indegree[next]--;
+            if(indegree[next] == 0) q.push(next);
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    solve();
+    return 0;
+}
