5c86cf1a5897df82d70c02a852a7a1f7d2e8cbcb
Fojla Rabbi <fojlarabbi018@gmail.com>
2026-03-24 15:45:45 +0600

    Create BFS.cpp

---
+++ Graph Theory/BFS.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+const int N = 1005;
+vector<int> adj[N];
+bool vis[N];
+int dist[N];
+
+void bfs(int src) {
+    queue<int> q;
+    q.push(src);
+    vis[src] = true;
+    dist[src] = 0;
+ 
+    while(!q.empty()) {
+        int u = q.front();
+        q.pop();
+
+        for(auto v : adj[u]) {
+            if(!vis[v]) {
+                q.push(v);
+                vis[v] = true;
+                dist[v] = dist[u] + 1;
+            }
+        }
+    }
+}
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    for(int i = 1; i <= m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    
+    bfs(1);
+    int cnt = 0;
+    for(int i = 1; i <= n; i++) {
+        cout << dist[i] << " ";
+    }
+    cout << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+    return 0;
+}
