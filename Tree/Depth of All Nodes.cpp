91f94be28866de21f0fd1c8e6e48cde13857407b
Fojla Rabbi <fojlarabbi018@gmail.com>
2026-03-25 11:24:37 +0600

    Update Depth of All Nodes.cpp

--- Tree/Depth of All Nodes.cpp
+++ Tree/Depth of All Nodes.cpp
@@ -1,1 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
 
+const int N = 1005;
+vector<int> adj[N];
+int depth[N];
+
+void dfs(int u, int p) {
+    for(auto v : adj[u]) {
+        if(v != p) {
+            depth[v] = depth[u] + 1;
+            dfs(v, u);
+        }
+    }
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    for(int i = 1; i < n; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    
+    dfs(1, -1);
+    for(int i = 1; i <= n; i++) {
+        cout << depth[i] << " ";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+    return 0;
+}
