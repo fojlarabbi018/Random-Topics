472edd3960b6813bed52f2ff024c5b89a1f74c3a
Fojla Rabbi <fojlarabbi018@gmail.com>
2026-06-08 12:06:19 +0600

    Update Exactly K Distinct Elements.cpp

--- Sliding Window (Two Pointers)/Exactly K Distinct Elements.cpp
+++ Sliding Window (Two Pointers)/Exactly K Distinct Elements.cpp
@@ -1,1 +1,61 @@
+/* 
+Given an array of size n, you need to find the number of subarrays 
+containing Exactly K distinct elements
+1 ≤ n ≤ 2⋅10^5, 1 ≤ ai ≤ 10^9
 
+
+Actually it is impossible to find that directly without n^2, but 
+what we can do is to find for "at most K" then just need to subtract "at most K - 1"
+
+Exactly(K) = Atmost(K) - Atmost(K - 1)
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int n, k;
+vector<int> a;
+
+ll atmost(int x) {
+    map<int, int> mp;
+    int l = 1;
+    int distinct = 0;
+    ll ans = 0;
+    // Fixing the right pointer r and 
+    // seeing how many subarrays are ending at r
+    for(int r = 1; r <= n; r++) {
+        if(mp.find(a[r]) == mp.end()) {
+            distinct++;
+            mp[a[r]]++;
+        }
+        else mp[a[r]]++;
+
+        while(distinct > x) {
+            mp[a[l]]--;
+            if(mp[a[l]] == 0) {
+                distinct--;
+                mp.erase(mp[a[l]]);
+            }
+            l++;
+        }
+        ans += r - l + 1;
+    }
+    return ans;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> k;
+    a.resize(n + 1);
+    for(int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+
+    ll ans = atmost(k) - atmost(k - 1);
+    cout << ans << '\n';
+
+    return 0;
+}
