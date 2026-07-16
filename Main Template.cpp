13cf76ef61e2dc4abec5d7302fa60c68b0d2f30b
Fojla Rabbi <fojlarabbi018@gmail.com>
2026-07-17 02:25:15 +0600

    Update Main Template.cpp

--- Main Template.cpp
+++ Main Template.cpp
@@ -291,15 +291,15 @@ void prime_fact(int n) {
 ////<======= Binary Exponentiation =======>
 
 const int N = 1e6 + 5, m = 1e9 + 7;
-int binpow(ll a, ll n, int m) {
+ll binpow(ll a, ll b, int m) {
     a = a % m;   
     ll res = 1 % m;
-    while(n > 0) {
+    while(b > 0) {
         if(n & 1) {
             res = res * a % m;
         }
         a = a * a % m;
-        n >>= 1;
+        b >>= 1;
     }
     return res;
 }
