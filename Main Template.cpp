072a77259b3e66af508e17cf29f1109faa178eb7
Fojla Rabbi <fojlarabbi018@gmail.com>
2026-07-27 21:34:13 +0600

    Update Main Template.cpp

--- Main Template.cpp
+++ Main Template.cpp
@@ -404,8 +404,10 @@ int nPr(int n, int r) { // O(1)
 
 struct Node {
     Node* next[2];
-    
+    int cnt;
+
     Node() {
+        cnt = 0;
         for(int i = 0; i < 2; i++) {
             next[i] = NULL;
         }
@@ -422,28 +424,48 @@ public:
 
     void insert(int num) {
         Node* cur = root;
+        cur->cnt++;
+
         for(int i = 30; i >= 0; i--) {
             int bit = (num >> i) & 1;
+
             if(cur->next[bit] == NULL) {
                 cur->next[bit] = new Node();
             }
+
             cur = cur->next[bit];
+            cur->cnt++;
+        }
+    }
+
+    void erase(int num) {
+        Node* cur = root;
+        cur->cnt--;
+
+        for(int i = 30; i >= 0; i--) {
+            int bit = (num >> i) & 1;
+            cur = cur->next[bit];
+            cur->cnt--;
         }
     }
 
     int get_max_xor(int num) {
         Node* cur = root;
         int ans = 0;
+
         for(int i = 30; i >= 0; i--) {
             int bit = (num >> i) & 1;
             int want = bit ^ 1;
 
-            if(cur->next[want] != NULL) {
+            if(cur->next[want] != NULL && cur->next[want]->cnt > 0) {
                 ans |= (1 << i);
                 cur = cur->next[want];
             }
-            else cur = cur->next[bit];
+            else {
+                cur = cur->next[bit];
+            }
         }
+
         return ans;
     }
 };
