405f362f6296ae604bd7fbadd33b5b4e4fbe206e
Fojla Rabbi <fojlarabbi018@gmail.com>
2026-07-27 21:39:09 +0600

    Update Binary Trie.cpp

--- Trie/Binary Trie.cpp
+++ Trie/Binary Trie.cpp
@@ -4,7 +4,10 @@ using namespace std;
 
 struct Node {
     Node* next[2];
+    int cnt;
+
     Node() {
+        cnt = 0;
         for(int i = 0; i < 2; i++) {
             next[i] = NULL;
         }
@@ -19,61 +22,50 @@ public:
         root = new Node();
     }
 
-    void insert(int num) { // O(32)
+    void insert(int num) {
         Node* cur = root;
+        cur->cnt++;
+
         for(int i = 30; i >= 0; i--) {
             int bit = (num >> i) & 1;
+
             if(cur->next[bit] == NULL) {
                 cur->next[bit] = new Node();
             }
+
             cur = cur->next[bit];
+            cur->cnt++;
         }
     }
 
-    int get_max_xor(int num) { // O(32)
+    void erase(int num) {
+        Node* cur = root;
+        cur->cnt--;
+
+        for(int i = 30; i >= 0; i--) {
+            int bit = (num >> i) & 1;
+            cur = cur->next[bit];
+            cur->cnt--;
+        }
+    }
+
+    int get_max_xor(int num) {
         Node* cur = root;
         int ans = 0;
+
         for(int i = 30; i >= 0; i--) {
             int bit = (num >> i) & 1;
             int want = bit ^ 1;
 
-            if(cur->next[want] != NULL) {
+            if(cur->next[want] != NULL && cur->next[want]->cnt > 0) {
                 ans |= (1 << i);
                 cur = cur->next[want];
             }
-            else cur = cur->next[bit];
+            else {
+                cur = cur->next[bit];
+            }
         }
+
         return ans;
     }
 };
-
-void solve() {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    Trie t;
-    for(int i = 0; i < n; i++) {
-        cin >> a[i];
-        t.insert(a[i]);
-    }
-
-    int mx = INT_MIN;
-    for(int i = 0; i < n; i++) {
-        int cur_mx = t.get_max_xor(a[i]);
-        mx = max(mx, cur_mx);
-    }
-
-    cout << mx << '\n';   
-}
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int t;
-    cin >> t;
-    while(t--) {
-        solve();
-    }
-    return 0;
-}
